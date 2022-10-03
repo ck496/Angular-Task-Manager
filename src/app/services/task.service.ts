db2bd57ae569fcf26e6e468761dc0a1999f79c9d
ck496 <chriskurian24@gmail.com>
2022-10-03 14:33:46 -0700

    Added code to delete tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Observable, of } from 'rxjs';
 import {Task} from "../Task";
 import {TASKS} from "../mock-tasks";
@@ -7,11 +8,17 @@ import {TASKS} from "../mock-tasks";
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    //need to append task id at the end of ApiUrl to delete
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url)
   }
 }

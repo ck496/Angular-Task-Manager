780aa2da5285c7cc3f0fcca38d608b3282ee8386
ck496 <chriskurian24@gmail.com>
2022-10-05 17:09:51 -0700

    Added a footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,7 +14,7 @@ export class HeaderComponent implements OnInit {
   subscription: Subscription;
 
   // In order to use a service you have to add it to the constructor, now you can do `this.uiService` and use it
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router:Router) {
     // Value is the the showAddTask boolean thats passed into the subject in uiService
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
   }
@@ -22,6 +24,10 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask(){
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string){
+    return this.router.url === route
   }
 
 }
